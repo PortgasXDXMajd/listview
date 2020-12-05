e332b72491f226158c3f946644514f18de95e510
PortgasXDXMajd <Majd.Kayyal.3398@hotmail.com>
2020-12-05 13:20:45 +0400

    constant file

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:listview/OnePersonWidget.dart';
+import 'package:listview/constant.dart';
 import 'package:listview/person.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,58 +9,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Person> myList = [
-    Person(
-        imagePath: 'assets/person1.jpg',
-        name: "Majd",
-        age: 22,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person2.jpg',
-        name: "Ahmed",
-        age: 35,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person3.jpeg',
-        name: "Ahlam",
-        age: 18,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person4.jpg',
-        name: "Salem",
-        age: 18,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person5.jpg',
-        name: "Samer",
-        age: 19,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person4.jpg',
-        name: "Linda",
-        age: 19,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person1.jpg',
-        name: "Aysha",
-        age: 25,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person3.jpeg',
-        name: "Amer",
-        age: 45,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person5.jpg',
-        name: "Amira",
-        age: 22,
-        isSelected: false),
-    Person(
-        imagePath: 'assets/person2.jpg',
-        name: "Osama",
-        age: 21,
-        isSelected: false),
-  ];
 
   List<Person> selectedPeople = List<Person>();
 
---
+++ lib/constant.dart
@@ -0,0 +1,54 @@
+import 'package:listview/person.dart';
+
+List<Person> myList = [
+    Person(
+        imagePath: 'assets/person1.jpg',
+        name: "Majd",
+        age: 22,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person2.jpg',
+        name: "Ahmed",
+        age: 35,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person3.jpeg',
+        name: "Ahlam",
+        age: 18,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person4.jpg',
+        name: "Salem",
+        age: 18,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person5.jpg',
+        name: "Samer",
+        age: 19,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person4.jpg',
+        name: "Linda",
+        age: 19,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person1.jpg',
+        name: "Aysha",
+        age: 25,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person3.jpeg',
+        name: "Amer",
+        age: 45,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person5.jpg',
+        name: "Amira",
+        age: 22,
+        isSelected: false),
+    Person(
+        imagePath: 'assets/person2.jpg',
+        name: "Osama",
+        age: 21,
+        isSelected: false),
+  ];
